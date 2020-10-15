fa5c2a22722c676651241e80127ff145ee5428b3
Francisco Veloz <franciscoveloz245@gmail.com>
2020-10-15 18:04:14 -0500

    El Analizador ahora clasifica las palabras reservadas

--- src/public/js/controller/manejadorLexico.js
+++ src/public/js/controller/manejadorLexico.js
@@ -1,18 +1,21 @@
-class Manejador {
-    constructor(num, tok, des, lin) {
+class Manejador
+{
+    constructor(num, tok, des, lin)
+    {
         this.numero = num
         this.token = tok
         this.descripcion = des
         this.linea = lin
-        this.token = []
-        this.coleccion = []
+        this.arreglo = []
     }
 
-    Separar(palabra) {
-        this.token = palabra.split("\n")
+    Separar(palabra)
+    {
+        this.arreglo = palabra.split(/[\n\s ]+/)
     }
 
-    Mostrar() {
+    Mostrar()
+    {
         return `
         <tr> 
             <td> ${this.numero} </td>
@@ -21,13 +24,6 @@ class Manejador {
             <td> ${this.linea} </td>
         </tr>`
     }
-
-    Generar() {
-        for (let i = 0; i < this.token.length; i++) {
-            this.coleccion.push(new Manejador(i + 1, this.token[i], "descripcion", i + 1))
-        }
-    }
-
 }
 
 export default Manejador
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,11 +1,12 @@
-import Manejador from './controller/manejadorLexico.js'
+import Manejador from './controller/manejadorLexico.js';
 
 const txtArea = document.getElementById("txtArea")
 const btnSeparar = document.getElementById("btnSeparar")
-const lineas = document.getElementById("lineas")
-
 
 const manejador = new Manejador()
+
+const reservadas = ["task", "main()", "new", "return", "PI"]
+let coleccion = []
 
 window.onload = () => {
     for (let i = 1; i < 20; i++) {
@@ -15,6 +16,21 @@ window.onload = () => {
 
 btnSeparar.addEventListener('click', () => {
     manejador.Separar(txtArea.value)
-    manejador.Generar()
-    document.getElementById("resultado").innerHTML += manejador.Mostrar()
+
+    for (let i = 0; i < manejador.arreglo.length; i++) {
+        coleccion.push(new Manejador(i+1, manejador.arreglo[i], ComprobarReservada(manejador.arreglo[i]), i+1))
+
+        document.getElementById("resultado").innerHTML += coleccion[i].Mostrar()
+    }
 })
+
+function ComprobarReservada(texto) {
+    let r
+    for (let i = 0; i < reservadas.length; i++) {
+        if(texto == reservadas[i]) {
+            r = "Palabra reservada"
+            i = manejador.arreglo.length
+        }
+    }
+    return r
+}
