aa7b02b129ae2d3dc74a76da9f3a58c5da6fe023
Francisco Veloz <franciscoveloz245@gmail.com>
2020-11-22 20:31:24 -0600

    Movimos todo lo del analizador lexico a su controlador

--- src/public/js/controller/controladorLexico.js
+++ src/public/js/controller/controladorLexico.js
@@ -5,6 +5,21 @@ class ControladorLexico {
     this.descripcion = des
     this.linea = lin
     this.arreglo = []
+    this.patronComparacion = /^[(]\w*\s*c|<|>|==|!=|<=\s*\w*[)]+$/i
+    this.patronMain = /^task main+[()]+$/i
+    this.patronLlaveInicio = /^\s*{+$/i
+    this.patronLlaveFin = /^\s*}+$/i
+    this.patronComentario = /^\s*[//]\w*/i
+    this.patronCadena = /^\s*var|const \w* = '\w*'+$/i
+    this.patronInstancia = /\s* var|const \w* = new \w*[(\w*|\d*)]+$/i
+    this.patronIf = /^\s*if[(\w*)]+$/
+    this.patronElse = /^\s*else+$/
+    this.patronWhile = /^\s*while[(\w*)]+$/
+    this.patronFunction = /^\s*function \w*[(\w*)]+$/
+    this.patronTiempo = /^\s*wait1msec[(\w*)]+$/
+    this.patronReservada = /^\s*new|Servo|Relay|Sensor|Motor|return|PI/
+    this.patronMetodo = /^click|run|off|position|on+$/i
+    this.anyFunction = /^\s*\w*[(]\w\w*[)]+$/i
   }
 
   Separar(palabra) {
@@ -36,6 +51,77 @@ class ControladorLexico {
     }
 
     document.getElementById(elemento).innerHTML = ""
+  }
+
+  AnalizadorLexico(arreglo, elemento) {
+    for (let i = 0; i < this.arreglo.length; i++) {
+
+      if (this.Comprobar(this.arreglo[i], this.patronMain) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Metodo principal", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronLlaveInicio) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Llave inicio", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronLlaveFin) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Llave fin", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronComentario) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Comentario", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronCadena) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Cadena", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronIf) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Declaración if", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronWhile) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Estructura de control", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronElse) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Declaración else", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronTiempo) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Metodo tiempo", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronFunction) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Función", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.anyFunction) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Función", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronComparacion) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Comparación", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronInstancia) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Instancia", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronMetodo) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Metodo", 1))
+      }
+
+      else if (this.Comprobar(this.arreglo[i], this.patronReservada) == true) {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "Palabra reservada", 1))
+      }
+
+      else {
+        arreglo.push(new ControladorLexico(i + 1, this.arreglo[i], "No identificado", 1))
+      }
+
+      document.getElementById(elemento).innerHTML += arreglo[i].Mostrar()
+    }
   }
 }
 
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -12,22 +12,6 @@ const btnCompilar = document.getElementById("btnCompilar")
 const lexico = new ControladorLexico()
 const ejemplos = new ControladorEjemplos()
 
-const patronComparacion = /^[(]\w*\s*c|<|>|==|!=|<=\s*\w*[)]+$/i
-const patronMain = /^task main+[()]+$/i
-const patronLlaveInicio = /^\s*{+$/i
-const patronLlaveFin = /^\s*}+$/i
-const patronComentario = /^\s*[//]\w*/i
-const patronCadena = /^\s*var|const \w* = '\w*'+$/i
-const patronInstancia = /\s* var|const \w* = new \w*[(\w*|\d*)]+$/i
-const patronIf = /^\s*if[(\w*)]+$/
-const patronElse = /^\s*else+$/
-const patronWhile = /^\s*while[(\w*)]+$/
-const patronFunction = /^\s*function \w*[(\w*)]+$/
-const patronTiempo = /^\s*wait1msec[(\w*)]+$/
-const patronReservada = /^\s*new|Servo|Relay|Sensor|Motor|return|PI/
-const patronMetodo = /^click|run|off|position|on+$/i
-const anyFunction = /^\s*\w*[(]\w\w*[)]+$/i
-
 let coleccion = []
 
 window.onload = () => {
@@ -40,8 +24,7 @@ btnCompilar.addEventListener('click', () => {
 
     lexico.LimpiarTabla(coleccion, "resultado")
     lexico.Separar(txtArea.value)
-
-    Lexico()
+    lexico.AnalizadorLexico(coleccion, "resultado")
 
     if (Sintactico()) {
         console.log('Correcto')
@@ -55,77 +38,6 @@ btnCompilar.addEventListener('click', () => {
         console.log('Error Sintactico: Faltante } después del cuerpo de la tarea principal')
     }
 })
-
-function Lexico() {
-    for (let i = 0; i < lexico.arreglo.length; i++) {
-
-        if (lexico.Comprobar(lexico.arreglo[i], patronMain) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Metodo principal", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronLlaveInicio) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Llave inicio", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronLlaveFin) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Llave fin", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronComentario) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Comentario", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronCadena) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Cadena", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronIf) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Declaración if", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronWhile) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Estructura de control", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronElse) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Declaración else", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronTiempo) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Metodo tiempo", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronFunction) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Función", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], anyFunction) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Función", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronComparacion) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Comparación", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronInstancia) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Instancia", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronMetodo) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Metodo", 1))
-        }
-
-        else if (lexico.Comprobar(lexico.arreglo[i], patronReservada) == true) {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "Palabra reservada", 1))
-        }
-
-        else {
-            coleccion.push(new ControladorLexico(i + 1, lexico.arreglo[i], "No identificado", 1))
-        }
-
-        document.getElementById("resultado").innerHTML += coleccion[i].Mostrar()
-    }
-}
 
 function Sintactico(descripcion = 'Metodo principal') {
     let res = false
